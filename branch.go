90270b6bc94c8c1e84f681693ae15ac9883b7a4d
dictator-x <dictator.xwu@gmail.com>
2020-02-11 11:34:57 -0500

    add branch.go

--- branch.go
+++ branch.go
@@ -5,6 +5,25 @@ import (
 	"io/ioutil"
 )
 
+func grade(score int) string {
+	g := ""
+	switch {
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("Wrong score: %d", score))
+	case score < 60:
+		g = "F"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score <= 100:
+		g = "A"
+	default:
+		panic(fmt.Sprintf("Wrong score: %d", score))
+	}
+	return g
+}
+
 func main() {
 	const filename = "abc.txt"
 	if contents, err := ioutil.ReadFile(filename); err != nil {
@@ -12,4 +31,12 @@ func main() {
 	} else {
 		fmt.Printf("%s\n", contents)
 	}
+	fmt.Println(
+		grade(0),
+		grade(59),
+		grade(60),
+		grade(82),
+		grade(99),
+		grade(100),
+	)
 }
