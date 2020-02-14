e623c0c7616289d825b603ec21f30d53c34e99a6
dictator-x <dictator.xwu@gmail.com>
2020-02-14 14:01:36 -0500

    add httpServer

---
+++ errhandling/defer/defer.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"eg/egolang/functional/fib"
+	"fmt"
+	"os"
+)
+
+func tryDefer() {
+	for i := 0; i < 100; i++ {
+		defer fmt.Println(i)
+		if i == 30 {
+			panic("printed too many")
+		}
+	}
+}
+
+func writeFile(filename string) {
+	file, err := os.OpenFile(filename, os.O_EXCL|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		if pathError, ok := err.(*os.PathError); !ok {
+			panic(err)
+		} else {
+			fmt.Println(pathError.Op, pathError.Path, pathError.Err)
+		}
+		return
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	f := fib.Fibonacci()
+	for i := 0; i < 20; i++ {
+		fmt.Fprintln(writer, f())
+	}
+}
+
+func main() {
+	// tryDefer()
+	writeFile("fib.txt")
+}
