ab19f8ae07e8b1eb3f71552d9f80c6fb13ae0631
dictator-x <dictator.xwu@gmail.com>
2020-02-14 16:02:12 -0500

    add userError

--- errhandling/filelistingserver/web.go
+++ errhandling/filelistingserver/web.go
@@ -11,11 +11,23 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
 		// Call actual handler.
 		err := handler(writer, request)
 		// Handler error.
 		if err != nil {
 			log.Printf("Error handling request: %s", err.Error())
+
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -31,8 +43,13 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HanderFileList))
+	http.HandleFunc("/", errWrapper(filelisting.HanderFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
