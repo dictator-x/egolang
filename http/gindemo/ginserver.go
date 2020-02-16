c9b3167d2685a2db0234100fd17304f9efe532f7
dictator-x <dictator.xwu@gmail.com>
2020-02-16 17:25:18 -0500

    demo http library

---
+++ http/gindemo/ginserver.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"math/rand"
+	"time"
+)
+
+const keyRequestId = "requestId"
+
+func main() {
+	r := gin.Default()
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	r.Use(func(c *gin.Context) {
+		// log path, latency, response code
+		s := time.Now()
+
+		c.Next()
+		logger.Info(
+			"incomming request",
+			zap.String("path", c.Request.URL.Path),
+			zap.Int("status", c.Writer.Status()),
+			zap.Duration("elapse", time.Now().Sub(s)),
+		)
+
+	}, func(c *gin.Context) {
+		c.Set("keyRequestId", rand.Int())
+		c.Next()
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+
+		h := gin.H{
+			"message": "pong",
+		}
+
+		if rid, exists := c.Get("keyRequestId"); exists {
+			h["keyRequestId"] = rid
+		}
+
+		c.JSON(200, h)
+	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(200, "hello")
+	})
+	r.Run()
+}
