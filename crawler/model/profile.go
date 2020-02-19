5f5095ad04b745c44a5a6f52c4c3049f490dd75c
dictator-x <dictator.xwu@gmail.com>
2020-02-18 22:42:05 -0500

    single thread crawl

--- crawler/model/profile.go
+++ crawler/model/profile.go
@@ -1,17 +1,25 @@
 package model
 
+import (
+	"fmt"
+)
+
 type Profile struct {
-	Name       string
-	Gender     string
-	Age        int
-	Height     int
-	Weight     int
-	Income     string
-	Marriage   string
-	Education  string
-	Occupation string
-	Hokou      string
-	Xinzuo     string
-	House      string
-	Car        string
+	Name string
+	// Gender     string
+	Age int
+	// Height     int
+	// Weight     int
+	// Income     string
+	// Marriage   string
+	// Education  string
+	// Occupation string
+	// Hokou      string
+	// Xinzuo     string
+	// House      string
+	// Car        string
 }
+
+// func (p *Profile) String() string {
+// 	return fmt.Sprintf("name: %s; age: %d", p.Name, p.Age)
+// }
